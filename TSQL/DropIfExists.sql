e6b286b1f0a298ea8d3ac308e53cc3d1106a49b2
Franklin Davenport <franklin@first1apps.com>
2015-12-21 13:23:22 -0600

    DropIfExists other ways

--- TSQL/DropIfExists.sql
+++ TSQL/DropIfExists.sql
@@ -1,3 +1,45 @@
+-- http://dattatreysindol.com/2012/05/29/tips-n-tricks-t-sql-an-elegant-way-to-create-or-alter-stored-procedures-in-one-go-for-easy-maintenance/
+
+--
+-- Create Dummy Objects if not exists and then ALTER
+--
+
+-- PROCEDURES
+IF OBJECT_ID('xxxxxxxxxxxxxxxxxxxxxxxxx') IS NULL
+EXEC('CREATE PROCEDURE xxxxxxxxxxxxxxxxxxxxxxxxx AS SET NOCOUNT ON;')
+GO
+
+
+
+--
+-- Drop Objects
+--
+
+-- TABLES
+IF OBJECT_ID('xxxxxxxxxxxxxxxxxxxxxxxxx') IS NOT NULL
+DROP TABLE xxxxxxxxxxxxxxxxxxxxxxxxx
+GO
+
+-- VIEWS 
+IF OBJECT_ID('xxxxxxxxxxxxxxxxxxxxxxxxx') IS NOT NULL
+DROP VIEW xxxxxxxxxxxxxxxxxxxxxxxxx
+GO
+
+-- FUNCTIONS
+IF OBJECT_ID('xxxxxxxxxxxxxxxxxxxxxxxxx') IS NOT NULL
+DROP FUNCTION xxxxxxxxxxxxxxxxxxxxxxxxx
+GO
+
+-- PROCEDURES
+IF OBJECT_ID('xxxxxxxxxxxxxxxxxxxxxxxxx') IS NOT NULL
+DROP PROCEDURE xxxxxxxxxxxxxxxxxxxxxxxxx
+GO
+
+
+
+--
+-- Old way that may have issues with schemas besides DBO
+--
 
 -- TABLES
 IF EXISTS (SELECT * FROM sys.objects WHERE type = 'U' AND name = 'xxxxxxxxxxxxxxxxxxxxxxxxx')
@@ -18,3 +60,4 @@ GO
 IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'xxxxxxxxxxxxxxxxxxxxxxxxx')
 DROP PROCEDURE xxxxxxxxxxxxxxxxxxxxxxxxx
 GO
+
