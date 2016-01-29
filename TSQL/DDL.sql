ef120af591f25bb43cf21e5da33103ba2fedd981
Franklin Davenport <franklin@first1apps.com>
2016-01-29 16:26:08 -0600

    TSQL DDL Templates

---
+++ TSQL/DDL.sql
@@ -0,0 +1,20 @@
+
+--
+-- Create Table
+--
+CREATE TABLE dbo.xxxxxxxxxxxxxxxxxxx (
+	xxxxxxxxxxxxxxxxxxxId int NOT NULL IDENTITY (1, 1),
+	ccccccccccccccccccc char(10) NOT NULL,
+	ddddddddddddddddddd datetime NOT NULL
+);
+
+--
+-- Primary Key
+--
+ALTER TABLE dbo.xxxxxxxxxxxxxxxxxxx
+	ADD CONSTRAINT PK_xxxxxxxxxxxxxxxxxxx PRIMARY KEY CLUSTERED (
+		xxxxxxxxxxxxxxxxxxxId
+	)
+;
+
+
